ecf9264410a021d9aefae17a6e97a4cf94fad9f3
hafidhadhiet <46853809+hafidhadhiet@users.noreply.github.com>
2022-01-08 15:31:19 +0700

    a

--- TugasBesar_Saham_APPL.py
+++ TugasBesar_Saham_APPL.py
@@ -1,107 +1,102 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[20]:
 
 
-# Data handling
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Bokeh libraries
-from bokeh.io import output_file, output_notebook, curdoc
+from bokeh.io import curdoc
 from bokeh.plotting import figure, show
-from bokeh.models import ColumnDataSource
-from bokeh.layouts import row, column, gridplot
-from bokeh.models.widgets import Tabs, Panel
-from bokeh.plotting import figure
 from bokeh.models import HoverTool, ColumnDataSource
+from bokeh.models import CategoricalColorMapper
+from bokeh.palettes import Spectral6
+from bokeh.layouts import widgetbox, row, gridplot, column
+from bokeh.models import Slider, Select
 
 
 # In[2]:
 
 
-#Read Data CSV
-df_stock = pd.read_csv("Volume.csv")
-print(df_stock.head())
+df_can = pd.read_excel('https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DV0101EN/labs/Data_Files/Canada.xlsx',
+                       sheet_name='Canada by Citizenship',
+                       skiprows=range(20),
+                       skipfooter=2
+                      )
 
 
 # In[3]:
 
 
-#Liat tipe data dari kolom Volume
-df_stock['Date'] = pd.to_datetime(df_stock.Date)
-print(df_stock.info())
+df_can.head()
 
 
 # In[4]:
 
 
-#Inisialisasi ke dataframe yang khusus menampung kolom volume
-df_appl = df_stock['Volume']
+df_can.OdName.unique()
 
 
 # In[5]:
 
 
-#Inisialisasi kedalam bentuk CDS agar dapat ditampilkan di figure
-cds_appl = ColumnDataSource(df_stock)
+# clean up the dataset to remove unnecessary columns (eg. REG) 
+df_can.drop(['AREA', 'REG', 'DEV', 'Type', 'Coverage'], axis=1, inplace=True)
 
+# let's rename the columns so that they make sense
+df_can.rename(columns={'OdName':'Country', 'AreaName':'Continent','RegName':'Region'}, inplace=True)
 
-# ## Pembuatan Figure
-# 
+# for sake of consistency, let's also make all column labels of type string
+df_can.columns = list(map(str, df_can.columns))
+
+# set the country name as index - useful for quickly looking up countries using .loc method
+df_can.set_index('Country', inplace=True)
+
+# add total column
+df_can['Total'] = df_can.sum(axis=1)
+
+# years that we will be using in this lesson - useful for plotting later on
+years = list(map(str, range(1980, 2014)))
+print('data dimensions:', df_can.shape)
+
 
 # In[6]:
 
 
-#Melakukan pembuatan figur dengan X-axis = Date dan Y-axis = Volume
-fig = figure(x_axis_type='datetime',
-                  plot_height=600,
-                  plot_width= 950,
-                  x_axis_label='Date',
-                  y_axis_label='Volume',
-                  title='Volume')
+df_can
 
-
-# In[7]:
-
-
-#Menentukan warna, dan source dari garis figur
-fig.line(x='Date', y='Volume', 
-        color='red', legend_label='APPL Volume',
-        source=df_stock)
-
-fig.legend.location = 'top_left'
-
-
-# ## Pembuatan Hover
-# 
 
 # In[8]:
 
 
-hov_appl = fig.circle(x='Date', y='Volume', source=cds_appl ,size=15, alpha=0, hover_fill_color='blue', hover_alpha=0.5)
+df_countries = df_can.loc[['Indonesia'],years].transpose()
+df_total = pd.DataFrame(df_countries.sum(axis=1))
+df_total.reset_index(inplace=True)
+df_total.columns = ["Tahun", "Jumlah Imigran"]
+df_total["Tahun"] = df_total["Tahun"].astype(int)
+
+df_total
 
 
-# In[9]:
+# In[16]:
 
 
-tooltips = [
-            ('Date', '@Date{%F}'),
-            ('Volume', '@Volume'),
-           ]
-fig.add_tools(HoverTool(tooltips=tooltips, formatters={'@Date': 'datetime'}, renderers=[hov_appl]))
+a = figure(title='Jumlah Imigran Asal Indonesia yang Masuk ke Kanada Pada Tahun 1970-2013', x_axis_label='Fertility (children per woman)', y_axis_label='Life Expectancy (years)',
+           plot_height=400, plot_width=700)
 
 
-# In[10]:
+# In[17]:
 
 
-curdoc().add_root(column(fig))
+a.line(x='Tahun', y='Jumlah Imigran', 
+        color='red', legend_label='APPL Volume',
+        source=df_total)
+
+a.legend.location = 'top_left'
 
 
-# In[11]:
+# In[22]:
 
 
-show(fig)
+curdoc().clear()
+curdoc().add_root(column(a))
 
